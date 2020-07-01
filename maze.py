2981cfbc377687559b24636b192fa815c3d9e2f0
tscheer100 <thomas.k.scheer@gmail.com>
2020-07-01 16:31:24 -0500

    drew grid, added cell object

--- maze.py
+++ maze.py
@@ -6,13 +6,16 @@ import pygame
 WIDTH = 500
 HEIGHT = 500
 FPS = 60
+SIZE = 40
+GRID_SIZE = 10
 
 pygame.init()
 pygame.mixer.init()
 
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("test")
+pygame.display.set_caption("Maze")
+
 
 clock = pygame.time.Clock()
 
@@ -20,6 +23,8 @@ run = True
 
 
 win.fill((255,255,255))
+
+dir_choice = ["north", "west", "east", "south"]
 
 def redrawGameWin():
     pygame.display.update()
@@ -31,15 +36,32 @@ class cell(object):
         self.x = x
         self.y = y
 
-        # n, w, e, s represents north, west, east, south repsectively  
-        self.n_wall = pygame.draw.line(win, (0, 0, 0), (self.x - 10, self.y - 10), (self.x + 10, self.y - 10), 5)
-        self.w_wall = pygame.draw.line(win, (0, 0, 0), (self.x - 10, self.y - 10), (self.x - 10, self.y + 10), 5)
-        self.e_wall = pygame.draw.line(win, (0, 0, 0), (self.x + 10, self.y - 10), (self.x + 10, self.y + 10), 5)
-        self.s_wall = pygame.draw.line(win, (0, 0, 0), (self.x - 10, self.y + 10), (self.x + 10, self.y + 10), 5)
+
+def drawGrid():
+    for row in range(1, GRID_SIZE + 1):
+        for col in range(1, GRID_SIZE + 1):
+            pygame.draw.line(win, (0,0,0), (row * SIZE, col * SIZE), (row * SIZE + SIZE, col * SIZE))
+            pygame.draw.line(win, (0,0,0), (row * SIZE, col * SIZE + SIZE), (row * SIZE, col * SIZE)) 
+
+    # finishing lines that the for loop didn't catch.        
+    pygame.draw.line(win, (0,0,0), (SIZE * GRID_SIZE + SIZE, SIZE), (SIZE * GRID_SIZE + SIZE, SIZE * GRID_SIZE + SIZE))
+    pygame.draw.line(win, (0,0,0), (SIZE, SIZE * GRID_SIZE + SIZE), (SIZE * GRID_SIZE + SIZE, SIZE * GRID_SIZE + SIZE))  
+drawGrid()
+
+grid = []
+
+def makeGrid():
+    for r in range(1, GRID_SIZE + 1):
+        for c in range(1, GRID_SIZE + 1):
+            grid.append(cell(r * SIZE, c * SIZE))
+            
+            
+makeGrid()
+for i in range(0, len(grid)):
+    print(grid[i].x, grid[i].y)            
 
 while run:
 
-    test_cell = cell(30,30)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
